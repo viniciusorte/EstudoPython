0077090f4bac40a7f136780b7088f379cc84e639
Vinícius <96997654+viniciusorte@users.noreply.github.com>
2022-01-07 01:09:26 +0000

    Concluido 3 etapa Ex 1 ao 15

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-salario=float(input('Qual salário do funcionário? R$'))
-
-#Valor dos 5% referente ao produto
-desconto=(15*salario/100)
-
-#Preço já com desconto
-sd=salario+desconto
-                                      
-print('Seu salário de R${}, aumentou para R${} pois aplicamos um aumento de 15% o que equivale a R${} a mais'.format(salario,sd,desconto))
+# Escreva um programa que pergunte a quantidade de Km percorridos por um carro alugado e a quantidade de dias pelos quais ele foi alugado. Calcule o preço a pagar, sabendo que o carro custa R$60 por dia e R$0,15 por Km rodado.
+km=float(input('Quanto Km rodados?'))
+dia=int(input('Quantos dias alugados?'))
+preço=(dia*60)+(km*0.15)
+print('Voce tem que pagar {:.2f} pois você alugou o carro por {} dias e andou {}km com ele'.format(preço,dia,km))
