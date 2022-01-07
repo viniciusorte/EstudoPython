c130b9a8315557a01d0dd097815f30f8c6f96d2a
Vinícius <96997654+viniciusorte@users.noreply.github.com>
2022-01-07 00:34:47 +0000

    Exercícios do 1 ao 13 feitos e corrigidos

--- main.py
+++ main.py
@@ -1,23 +1,9 @@
-#Preço antigo   % * peso/100
-gar=(5.50)
-dan=(4.90)
-pao=(0.30)
-bif=(50.0)
+salario=float(input('Qual salário do funcionário? R$'))
 
 #Valor dos 5% referente ao produto
-dg=(5*gar/100)
-dd=(5*dan/100)
-dp=(5*pao/100)
-db=(5*bif/100)
+desconto=(15*salario/100)
 
 #Preço já com desconto
-pg=gar-dg
-pd=dan-dd
-pp=pao-dp
-pb=bif-db                                       
-
-print('Siga a lista de descontos:') 
-print('Garrafa estava custando {:.2f}, com 5% de desconto agora está custando R${:.2f} '.format(gar,pg))
-print('Danoninho estava custando {:.2f}, com 5% de desconto agora está custando{:.2f}'.format(dan,pd))
-print('Pãozinho estava custando {:.2f}, com 5% de desconto agora está custando R${:.2f}'.format(pao,pp))
-print('Bifão estava custando {:.2f}, com 5% de desconto agora está custando R${:.2f}'.format(bif,pb))
+sd=salario+desconto
+                                      
+print('Seu salário de R${}, aumentou para R${} pois aplicamos um aumento de 15% o que equivale a R${} a mais'.format(salario,sd,desconto))
