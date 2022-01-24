6fd769e4beee6400361b2260d1824983d564536c
Vinícius <96997654+viniciusorte@users.noreply.github.com>
2022-01-24 22:45:57 +0000

    Exercicios do 16 ao 21 corrigidos

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-#Leia um ângulo qualquer e mostre na tela o valor de Seno,Cosseno e Tangente desse ângulo
-
-import math
-a=float(input('Quanto mede o cateto oposto?'))
-b=float(input('Quando mede o cateto adjacente?'))
-r=(a**2)+(b**2)
-h=r**(1/2)
-print('A hipotenusa mede {:.2f}'.format(h))
-
+import random
+a1 = input('Primeiro aluno: ')
+a2 = input('Segundo aluno: ')
+a3 = input('Terceiro aluno: ')
+a4 = input('Quarto aluno: ')
+lista = [a1, a2, a3, a4]
+random.shuffle(lista)
+print('Ordem de apresentação:')
+print(lista)
